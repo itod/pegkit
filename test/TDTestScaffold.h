f6c8cd21e8c420252e4ea30b6097622676c1260d
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-03-23 10:51:03 -0500

    * fix unit test bundle

--- test/TDTestScaffold.h
+++ test/TDTestScaffold.h
@@ -17,13 +17,14 @@
 #import <PEGKit/PEGKit.h>
 #import <OCMock/OCMock.h>
 
-#define TDTrue(e) STAssertTrue((e), @"")
-#define TDFalse(e) STAssertFalse((e), @"")
-#define TDNil(e) STAssertNil((e), @"")
-#define TDNotNil(e) STAssertNotNil((e), @"")
-#define TDEquals(e1, e2) STAssertEquals((e1), (e2), @"")
-#define TDEqualObjects(e1, e2) STAssertEqualObjects((e1), (e2), @"")
+#define TDTrue(e) XCTAssertTrue((e), @"")
+#define TDFalse(e) XCTAssertFalse((e), @"")
+#define TDNil(e) XCTAssertNil((e), @"")
+#define TDNotNil(e) XCTAssertNotNil((e), @"")
+#define TDEquals(e1, e2) XCTAssertEqual((e1), (e2), @"")
+#define TDEqualObjects(e1, e2) XCTAssertEqualObjects((e1), (e2), @"")
 
 #define TD_EMIT 1
 
-#define VERIFY() @try { [_mock verify]; } @catch (NSException *ex) { STAssertTrue(0, [ex reason]); }
+#define VERIFY()     @try { [_mock verify]; } @catch (NSException *ex) { NSString *msg = [ex reason]; XCTAssertTrue(0, @"%@", msg); }
+
